29070448ea99713b1fc7784b74d1db65429befc1
GeorgiiBurdinComo <georgii.burdin@gocomo.io>
2024-06-30 23:36:49 +0200

    Initial Commit

---
+++ broadcast_attention/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  broadcast_attention
+//
+//  Created by Georgii Burdi on 30.06.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ broadcast_attention/broadcast_attentionApp.swift
@@ -0,0 +1,17 @@
+//
+//  broadcast_attentionApp.swift
+//  broadcast_attention
+//
+//  Created by Georgii Burdi on 30.06.24.
+//
+
+import SwiftUI
+
+@main
+struct broadcast_attentionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
